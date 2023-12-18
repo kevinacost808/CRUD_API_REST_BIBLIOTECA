8cf9ef3ecdcd30d94fbc0ba58added6800e1992b
kevin Acosta <kevinacost808@gmail.com>
2023-12-17 23:54:59 -0500

    commit inicial

---
+++ src/main/java/com/kevin/apiRestBibliotecaLibrosBidereccional/ApiRestBibliotecaLibrosBidereccionalApplication.java
@@ -0,0 +1,13 @@
+package com.kevin.apiRestBibliotecaLibrosBidereccional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestBibliotecaLibrosBidereccionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestBibliotecaLibrosBidereccionalApplication.class, args);
+	}
+
+}
